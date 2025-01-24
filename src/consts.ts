1f92a48cfaa62a122b7401ad94390d79e8d4331e
itskitto <kitto.khr@gmail.com>
2025-01-24 16:21:03 -0500

    clean metadata and routing

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "kitto";
+export const SITE_DESCRIPTION =
+  "A landing pad for all my random thoughts on the tech, the web, and apps";
